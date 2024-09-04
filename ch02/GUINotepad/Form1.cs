3cf740b9cce626584bdade9aed4eb6091e480eca
Henry Cahill <hlcmailval2021@gmail.com>
2024-09-04 16:49:54 -0500

    Chapter-2-Part-C

---
+++ ch02/GUINotepad/Form1.cs
@@ -0,0 +1,10 @@
+namespace NotepadGUI
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+   }
+}
---
+++ ch02/GUIRadio/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Radio
+{
+   partial class Form1
+   {
+      /// <summary>
+      ///  Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///  Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      ///  Required method for Designer support - do not modify
+      ///  the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         groupBox1 = new GroupBox();
+         button1 = new Button();
+         button2 = new Button();
+         button3 = new Button();
+         button4 = new Button();
+         button5 = new Button();
+         button6 = new Button();
+         groupBox2 = new GroupBox();
+         checkBox1 = new CheckBox();
+         checkBox2 = new CheckBox();
+         groupBox3 = new GroupBox();
+         checkBox3 = new CheckBox();
+         trackBar1 = new TrackBar();
+         groupBox4 = new GroupBox();
+         label1 = new Label();
+         radioButton1 = new RadioButton();
+         radioButton2 = new RadioButton();
+         button7 = new Button();
+         pictureBox1 = new PictureBox();
+         groupBox1.SuspendLayout();
+         groupBox2.SuspendLayout();
+         groupBox3.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+         groupBox4.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+         SuspendLayout();
+         // 
+         // groupBox1
+         // 
+         groupBox1.Controls.Add(button6);
+         groupBox1.Controls.Add(button5);
+         groupBox1.Controls.Add(button4);
+         groupBox1.Controls.Add(button3);
+         groupBox1.Controls.Add(button2);
+         groupBox1.Controls.Add(button1);
+         groupBox1.Location = new Point(55, 59);
+         groupBox1.Name = "groupBox1";
+         groupBox1.Size = new Size(597, 193);
+         groupBox1.TabIndex = 0;
+         groupBox1.TabStop = false;
+         groupBox1.Text = "Pre-set Stations";
+         // 
+         // button1
+         // 
+         button1.Location = new Point(28, 67);
+         button1.Name = "button1";
+         button1.Size = new Size(84, 79);
+         button1.TabIndex = 0;
+         button1.Text = "1";
+         button1.UseVisualStyleBackColor = true;
+         // 
+         // button2
+         // 
+         button2.Location = new Point(118, 67);
+         button2.Name = "button2";
+         button2.Size = new Size(84, 79);
+         button2.TabIndex = 1;
+         button2.Text = "2";
+         button2.UseVisualStyleBackColor = true;
+         // 
+         // button3
+         // 
+         button3.Location = new Point(208, 67);
+         button3.Name = "button3";
+         button3.Size = new Size(84, 79);
+         button3.TabIndex = 2;
+         button3.Text = "3";
+         button3.UseVisualStyleBackColor = true;
+         // 
+         // button4
+         // 
+         button4.Location = new Point(298, 67);
+         button4.Name = "button4";
+         button4.Size = new Size(84, 79);
+         button4.TabIndex = 3;
+         button4.Text = "4";
+         button4.UseVisualStyleBackColor = true;
+         // 
+         // button5
+         // 
+         button5.Location = new Point(388, 67);
+         button5.Name = "button5";
+         button5.Size = new Size(84, 79);
+         button5.TabIndex = 4;
+         button5.Text = "5";
+         button5.UseVisualStyleBackColor = true;
+         // 
+         // button6
+         // 
+         button6.Location = new Point(478, 67);
+         button6.Name = "button6";
+         button6.Size = new Size(84, 79);
+         button6.TabIndex = 5;
+         button6.Text = "6";
+         button6.UseVisualStyleBackColor = true;
+         // 
+         // groupBox2
+         // 
+         groupBox2.Controls.Add(checkBox2);
+         groupBox2.Controls.Add(checkBox1);
+         groupBox2.Location = new Point(674, 59);
+         groupBox2.Name = "groupBox2";
+         groupBox2.Size = new Size(363, 193);
+         groupBox2.TabIndex = 1;
+         groupBox2.TabStop = false;
+         groupBox2.Text = "Speakers";
+         // 
+         // checkBox1
+         // 
+         checkBox1.AutoSize = true;
+         checkBox1.Location = new Point(42, 87);
+         checkBox1.Name = "checkBox1";
+         checkBox1.Size = new Size(137, 52);
+         checkBox1.TabIndex = 0;
+         checkBox1.Text = "Rear";
+         checkBox1.UseVisualStyleBackColor = true;
+         // 
+         // checkBox2
+         // 
+         checkBox2.AutoSize = true;
+         checkBox2.Location = new Point(194, 87);
+         checkBox2.Name = "checkBox2";
+         checkBox2.Size = new Size(150, 52);
+         checkBox2.TabIndex = 1;
+         checkBox2.Text = "Front";
+         checkBox2.UseVisualStyleBackColor = true;
+         // 
+         // groupBox3
+         // 
+         groupBox3.Controls.Add(trackBar1);
+         groupBox3.Controls.Add(checkBox3);
+         groupBox3.Location = new Point(61, 312);
+         groupBox3.Name = "groupBox3";
+         groupBox3.Size = new Size(591, 210);
+         groupBox3.TabIndex = 2;
+         groupBox3.TabStop = false;
+         groupBox3.Text = "Volume Control";
+         // 
+         // checkBox3
+         // 
+         checkBox3.AutoSize = true;
+         checkBox3.Location = new Point(31, 81);
+         checkBox3.Name = "checkBox3";
+         checkBox3.Size = new Size(149, 52);
+         checkBox3.TabIndex = 0;
+         checkBox3.Text = "Mute";
+         checkBox3.UseVisualStyleBackColor = true;
+         // 
+         // trackBar1
+         // 
+         trackBar1.Location = new Point(186, 69);
+         trackBar1.Name = "trackBar1";
+         trackBar1.Size = new Size(333, 136);
+         trackBar1.TabIndex = 1;
+         // 
+         // groupBox4
+         // 
+         groupBox4.Controls.Add(radioButton2);
+         groupBox4.Controls.Add(radioButton1);
+         groupBox4.Controls.Add(label1);
+         groupBox4.Location = new Point(676, 329);
+         groupBox4.Name = "groupBox4";
+         groupBox4.Size = new Size(369, 185);
+         groupBox4.TabIndex = 3;
+         groupBox4.TabStop = false;
+         groupBox4.Text = "Tuning";
+         // 
+         // label1
+         // 
+         label1.AutoSize = true;
+         label1.BackColor = Color.Black;
+         label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+         label1.ForeColor = Color.Silver;
+         label1.Location = new Point(32, 67);
+         label1.Name = "label1";
+         label1.Size = new Size(125, 65);
+         label1.TabIndex = 0;
+         label1.Text = "92.9";
+         // 
+         // radioButton1
+         // 
+         radioButton1.AutoSize = true;
+         radioButton1.Location = new Point(186, 51);
+         radioButton1.Name = "radioButton1";
+         radioButton1.Size = new Size(119, 52);
+         radioButton1.TabIndex = 1;
+         radioButton1.TabStop = true;
+         radioButton1.Text = "AM";
+         radioButton1.UseVisualStyleBackColor = true;
+         // 
+         // radioButton2
+         // 
+         radioButton2.AutoSize = true;
+         radioButton2.Location = new Point(186, 109);
+         radioButton2.Name = "radioButton2";
+         radioButton2.Size = new Size(114, 52);
+         radioButton2.TabIndex = 2;
+         radioButton2.TabStop = true;
+         radioButton2.Text = "FM";
+         radioButton2.UseVisualStyleBackColor = true;
+         // 
+         // button7
+         // 
+         button7.Location = new Point(1114, 67);
+         button7.Name = "button7";
+         button7.Size = new Size(328, 212);
+         button7.TabIndex = 4;
+         button7.Text = "Power\r\nOn/Off";
+         button7.UseVisualStyleBackColor = true;
+         // 
+         // pictureBox1
+         // 
+         pictureBox1.Image = Properties.Resources.MusicNote;
+         pictureBox1.Location = new Point(1192, 329);
+         pictureBox1.Name = "pictureBox1";
+         pictureBox1.Size = new Size(156, 213);
+         pictureBox1.TabIndex = 5;
+         pictureBox1.TabStop = false;
+         // 
+         // Form1
+         // 
+         AutoScaleDimensions = new SizeF(20F, 48F);
+         AutoScaleMode = AutoScaleMode.Font;
+         ClientSize = new Size(1524, 751);
+         Controls.Add(pictureBox1);
+         Controls.Add(button7);
+         Controls.Add(groupBox4);
+         Controls.Add(groupBox3);
+         Controls.Add(groupBox2);
+         Controls.Add(groupBox1);
+         Name = "Form1";
+         Text = "Radio";
+         groupBox1.ResumeLayout(false);
+         groupBox2.ResumeLayout(false);
+         groupBox2.PerformLayout();
+         groupBox3.ResumeLayout(false);
+         groupBox3.PerformLayout();
+         ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+         groupBox4.ResumeLayout(false);
+         groupBox4.PerformLayout();
+         ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+         ResumeLayout(false);
+      }
+
+      #endregion
+
+      private GroupBox groupBox1;
+      private Button button6;
+      private Button button5;
+      private Button button4;
+      private Button button3;
+      private Button button2;
+      private Button button1;
+      private GroupBox groupBox2;
+      private CheckBox checkBox2;
+      private CheckBox checkBox1;
+      private GroupBox groupBox3;
+      private TrackBar trackBar1;
+      private CheckBox checkBox3;
+      private GroupBox groupBox4;
+      private RadioButton radioButton2;
+      private RadioButton radioButton1;
+      private Label label1;
+      private Button button7;
+      private PictureBox pictureBox1;
+   }
+}
