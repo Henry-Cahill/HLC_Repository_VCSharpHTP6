9e89384a174f9640e8cb09f9529ec4a7d9a00ad2
Henry Cahill <hlcmailval2021@gmail.com>
2024-09-05 10:49:48 -0500

    Chapter-3-Part-K

---
+++ ch03/Chp3.FigureA-Welcome2/Welcome2.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Figure 3.10: Welcome2.cs
+//Displaying one line of text with multiple statements.
+
+using System;
+
+class Welcome2
+{
+   // Main Method begins execution of C# application.
+   static void Main()
+   {
+      Console.Write("Welcome to");
+      Console.WriteLine("C# Programming");
+   }//end Main
+}//end class Welcome2
+
---
+++ ch03/Chp3.FigureA-Welcome3/Welcome3.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Figure 3.11: Welcome3.cs
+//Displaying multiple lines with a single statement.
+using System;
+
+class Welcome3
+{
+   // Main Method begins execution of C# application.
+   static void Main()
+   {
+      Console.WriteLine("Welcome\nto\nC#\nProgramming!");
+   }//end Main
+}//end class Welcome3
