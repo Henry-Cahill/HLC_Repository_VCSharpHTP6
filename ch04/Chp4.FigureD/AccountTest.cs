616338d604ab8d5a5d30913d2dfec65afacac53e
Henry Cahill <hlcmailval2021@gmail.com>
2024-09-05 11:57:46 -0500

    Chapter-4-Part-F

---
+++ ch04/Chp4.FigureD/AccountTest.cs
@@ -0,0 +1,44 @@
+﻿//Figure 4.12: AccountTest.cs
+//Reading and writing monetary amounts with Account objects.
+
+using System;
+
+class AccountTest
+{
+   static void Main()
+   {
+      Account account1 = new Account("Jane Green", 50.00m);
+      Account account2 = new Account("John Blue", -7.53m);
+      // display initial balance of each object
+
+      Console.WriteLine(
+         $"{account1.Name}'s balance: {account1.Balance:C}");
+      Console.WriteLine(
+         $"{account2.Name}'s balance: {account2.Balance:C}");
+
+      //prompt for then read input
+      Console.Write("\nEnter deposit amount for account1: ");
+      decimal depositAmount = decimal.Parse(Console.ReadLine());
+      Console.WriteLine(
+         $"adding {depositAmount:C} to account1 balance\n");
+      account1.Deposit(depositAmount); // add to account1's balance
+
+      Console.WriteLine(
+         $"{account1.Name}'s balance: {account1.Balance:C}");
+      Console.WriteLine(
+         $"{account2.Name}'s balance: {account2.Balance:C}");
+
+      //prompt for then read input
+      Console.Write("\nEnter deposit amount for account2: ");
+      depositAmount = decimal.Parse(Console.ReadLine());
+      Console.WriteLine(
+         $"adding {depositAmount:C} to account2 balance\n");
+      account2.Deposit(depositAmount); // add to account2's balance
+
+      //display balances
+      Console.WriteLine(
+         $"{account1.Name}'s balance: {account1.Balance:C}");
+      Console.WriteLine(
+         $"{account2.Name}'s balance: {account2.Balance:C}");
+   }
+}
