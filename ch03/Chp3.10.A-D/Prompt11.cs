0d3e95da19da32f7ed8f13ff128d4ff08e837d04
Henry Cahill <hlcmailval2021@gmail.com>
2024-09-05 10:17:27 -0500

    Chapter-3-Part-E

---
+++ ch03/Chp3.10.A-D/Prompt11.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Prompt11
+{
+   class Prompt11
+   {
+      //Main method begins execution of C# application
+      static void Main()
+      {
+         int x = 2;
+         int y = 3;
+
+         Console.WriteLine($"x = {x}");
+         Console.WriteLine($"Value of {x} + {x} is {x + x}");
+         Console.WriteLine("x =");
+         Console.WriteLine($"{x + y} = {x + y}");
+
+      }//end Main
+   }//end class Prompt11
+}
