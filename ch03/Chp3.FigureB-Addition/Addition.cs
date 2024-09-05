26572e7e5557308efd1db9311ecdd64b89175d2c
Henry Cahill <hlcmailval2021@gmail.com>
2024-09-05 10:52:43 -0500

    Chapter-3-Part-L

---
+++ ch03/Chp3.FigureB-Addition/Addition.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Figure 3.14: Addition.cs
+//Displaying the sum of two numbers input from the keyboard.
+using System;
+
+class Addition
+{
+   // Main Method begins execution of C# application.
+   static void Main()
+   {
+      int number1; //declare first number to add
+      int number2; //declare second number to add
+      int sum; // declare sum of number1 and number2
+
+      Console.Write("Enter first integer: "); //prompt user
+      // read fist number from user
+      number1 = int.Parse(Console.ReadLine());
+      Console.Write("Enter second integer: "); //prompt user
+      //read second number from user
+      number2 = int.Parse(Console.ReadLine());
+      
+      sum = number1 + number2; //add numbers
+
+      Console.WriteLine($"Sum is {sum}"); // display sum
+   }//end Main
+}//end class Addition
---
+++ ch03/Chp3.FigureC-Comparison/Comparison.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Figure 3.22: Comparison.cs
+//Comparing integers using if statements, equality operators
+// and relational operators.
+using System;
+
+class Comparison
+{
+   // Main Method begins execution of C# application.
+   static void Main()
+   {
+      //Prompt user and read first number
+      Console.Write("Enter first intiger: ");
+      int number1 = int.Parse(Console.ReadLine());
+
+      //Prompt user and read second number
+      Console.Write("Enter second intiger: ");
+      int number2 = int.Parse(Console.ReadLine());
+
+      if (number1 == number2)
+      {
+         Console.WriteLine($"{number1} == {number2}");
+      }
+
+      if (number1 != number2)
+      {
+         Console.WriteLine($"{number1} != {number2}");
+      }
+
+      if (number1 < number2)
+      {
+         Console.WriteLine($"{number1} < {number2}");
+      }
+
+      if (number1 > number2)
+      {
+         Console.WriteLine($"{number1} > {number2}");
+      }
+
+      if (number1 <= number2)
+      {
+         Console.WriteLine($"{number1} <= {number2}");
+      }
+
+      if (number1 >= number2)
+      {
+         Console.WriteLine($"{number1} >= {number2}");
+      }
+   }//end Main
+}//end class Comparison
