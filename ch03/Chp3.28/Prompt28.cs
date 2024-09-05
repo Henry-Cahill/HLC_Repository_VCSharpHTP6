0591bf4ed9d97f3245b0db255fca81c68f4c6ae4
Henry Cahill <hlcmailval2021@gmail.com>
2024-09-05 10:39:59 -0500

    Chapter-3-Part-I

---
+++ ch03/Chp3.28/Prompt28.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Figure 3.28: Prompt28.cs
+//(Digits of an Integer) Write an app that inputs one number consisting of five digits from the user, separates the number into its individual digits and displays the digits separated from one another by three spaces each. For example, if the user types 42339, the application should display.
+//4   2   3   3   9
+// Assume the user enters the correct number of digits.
+// What happens when you execute the application and type a number with more than five digits?
+// What happens when you execute the application and type a number with fewer than five digits?
+//[Hint: It's possible to do this exercise with the techniques you learned in this chapter. You'll need to use both division and remainder operations to "pick off" each digit.] 
+
+using System;
+using System.Numerics;
+
+class Prompt28
+{
+   // Main Method begins execution of C# application.
+   static void Main()
+   {
+      //Prompt user and read first number
+      Console.Write("Enter an intiger of five digits: ");
+      int a = int.Parse(Console.ReadLine());
+
+      int aa = (a / 10000);
+      int ab = ((a - (aa * 10000)) / 1000);
+      int ac = ((a - (aa * 10000) - (ab * 1000)) / 100);
+      int ad = ((a - (aa * 10000) - (ab * 1000) - (ac * 100)) / 10);
+      int ae = ((a - (aa * 10000) - (ab * 1000) - (ac * 100) - (ad * 10)) / 1);
+      Console.WriteLine($"{aa}   {ab}   {ac}   {ad}   {ae}");
+
+
+   }//end Main
+}//end class Prompt28
---
+++ ch03/Chp3.30/Prompt30.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Figure 3.30
+//(Counting Negative, Positive and Zero Values)
+//Write an application that inputs five numbers and determines and displays the number of negative numbers input, the number of positive numbers input and the number of zeros input.
+
+using System;
+using System.Numerics;
+
+class Prompt30
+{
+   // Main Method begins execution of C# application.
+   static void Main()
+   {
+      //Prompt user and read first number
+      Console.Write("Enter an intiger of five digits: ");
+      int a = int.Parse(Console.ReadLine());
+
+      int aa = (a / 10000);
+      int ab = ((a - (aa * 10000)) / 1000);
+      int ac = ((a - (aa * 10000) - (ab * 1000)) / 100);
+      int ad = ((a - (aa * 10000) - (ab * 1000) - (ac * 100)) / 10);
+      int ae = ((a - (aa * 10000) - (ab * 1000) - (ac * 100) - (ad * 10)) / 1);
+      Console.WriteLine($"{aa}   {ab}   {ac}   {ad}   {ae}");
+
+      if       (0 == aa)         Console.WriteLine($"{aa} is an zero number");
+      else if  (0 == (aa % 2))   Console.WriteLine($"{aa} is an even number.");
+      else if  (0 != (aa % 2))   Console.WriteLine($"{aa} is an odd number.");
+
+      if       (0 == ab)         Console.WriteLine($"{ab} is an zero number");
+      else if  (0 == (ab % 2))   Console.WriteLine($"{ab} is an even number.");
+      else if  (0 != (ab % 2))   Console.WriteLine($"{ab} is an odd number.");
+
+      if       (0 == ac)         Console.WriteLine($"{ac} is an zero number");
+      else if  (0 == (ac % 2))   Console.WriteLine($"{ac} is an even number.");
+      else if  (0 != (ac % 2))   Console.WriteLine($"{ac} is an odd number.");
+
+      if       (0 == ad)         Console.WriteLine($"{ad} is an zero number");
+      else if  (0 == (ad % 2))   Console.WriteLine($"{ad} is an even number.");
+      else if  (0 != (ad % 2))   Console.WriteLine($"{ad} is an odd number.");
+
+      if       (0 == ae)         Console.WriteLine($"{ae} is an zero number");
+      else if  (0 == (ae % 2))   Console.WriteLine($"{ae} is an even number.");
+      else if  (0 != (ae % 2))   Console.WriteLine($"{ae} is an odd number.");
+      
+
+   }//end Main
+}//end class Prompt30
