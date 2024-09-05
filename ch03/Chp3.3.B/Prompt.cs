bd256beeecccd1c0791e363c6cb76c6b2d1fc26c
Henry Cahill <hlcmailval2021@gmail.com>
2024-09-05 09:55:55 -0500

    Chapter-3-Part-B

---
+++ ch03/Chp3.3.B/Prompt.cs
@@ -0,0 +1,21 @@
+﻿// Self Review Assignment 3.3B: Prompt.cs
+// Prompt the user to enter an integer.
+using System;
+
+namespace Prompt
+{
+   class Prompt
+   {
+      //Main method begins execution of C# application
+      static void Main()
+      {
+         int number; //declare first number to add
+
+
+         Console.Write("Enter an integer: "); //prompt user
+                                                 // read fist number from user
+         number = int.Parse(Console.ReadLine());
+         Console.WriteLine($"{number} is the Number the user entered after Prompted"); // display prompted number
+      }//end Main
+   }//end class Prompt
+}
---
+++ ch03/Chp3.3.C/Prompt2.cs
@@ -0,0 +1,21 @@
+﻿// Self Review Assignment 3.3C: Prompt.cs
+// Input an integer and assign the result to int variable value. 
+using System;
+
+namespace Prompt2
+{
+   class Prompt2
+   {
+      //Main method begins execution of C# application
+      static void Main()
+      {
+         int number; //declare first number
+
+
+         Console.Write("Enter an integer: "); //prompt user
+                                              // read fist number from user
+         number = int.Parse(Console.ReadLine());
+         Console.WriteLine($"{number} is the Input Integer the user entered after Prompted. While being held within a int. variable."); // display prompted number
+      }//end Main
+   }//end class Prompt2
+}
